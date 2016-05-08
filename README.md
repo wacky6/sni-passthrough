57ef8f93db8539bd6425a892ecccc52cd930ab21
wacky6.AriesMBP <416707889@qq.com>
2016-05-09 01:22:54 +0800

    rework internals

--- README.md
+++ README.md
@@ -1,54 +1,59 @@
 sni-passthrough
 ===
-A minimalistic SNI proxy.
+A minimalistic SNI pass-through proxy. **Not** a full featured TLS termination or load-balancer!
 
 ## Usage
 ```JavaScript
-    require('sni-passthrough').createServer({
-        sni: {
-            'sni-1.com': '127.0.0.1:444',
-            'sni-2.com': '127.0.0.1:445',
-            'sni-3.com': '[::1]:446',
-        },
-        fallback: '127.0.0.1:444'
-    })
-    .listen(443)
+require('sni-passthrough').createServer({
+    sni: {
+        'sni-1.com': '127.0.0.1:444',
+        'sni-2.com': '127.0.0.1:445',
+        'sni-3.com': '[::1]:446',
+        '*':         '127.0.0.1:444'
+    }
+})
+.listen(443)
 ```
 
 ## API
 
 #### createServer(opts) => net.Server
 ###### opts.sni
-hostname => destination mapping
+Either an object or a function
 
-destination is `"host:port"` string, port must **NOT** be omitted.  
-Wildcard and RegExp are not supported.
+**The object**: hostname => destination mapping  
+destination is a `"host:port"` string, port must **NOT** be omitted.  
+Wildcard and RegExp are not supported.  
+You can use hostname `'*'` to designate a fallback.
 
-###### opts.fallback
-fallback destination
-
-If `fallback` is not provided, and no SNI destination is found,
-incoming connection will be closed (`socket.destroy()`).
-
+**The function**: `(hostname) => ({host, port}) || undefined`  
+If SNI is not sent, `hostname` will be `null`  
+If function returns `undefined`, incoming connection is dropped.  
+You can use this to implement load-balancing or complicated logic.
 
 `opts` will be passed to `net.createServer`, you can specify additional options
 
 ###### Event: 'forward'
 returned server emits 'forward' event:
-
 ```JavaScript
-    passthourghSvr.on('forward', (hostname, {host, port}) => {
-        // hostname => SNI server name
-        // host     => destination host or `null`
-        // port     => destination port or `null`
-    })
+passthourgh.on('forward', (hostname, {host, port}) => {
+    // hostname => SNI server name
+    // host     => destination host
+    // port     => destination port
+})
 ```
 
 ## Caveat
-Backend nodes won't get remote peer's ip address.
+Backends won't get remote peer's ip address.
+
+Maybe a `X-Forwarded-For` TLS extension? 🙃
 
 ## Performance
-Not tested yet. :(
+On loopback, with my 2015 MBP @ 2.4Ghz, I was able to achieve:
+
+* ~8Ghz thorough-put
+* ~1800 QPS for a single 2000 QPS backend (~10% loss, `wrk -c100`)
+
 
 ## License
 MIT
--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 
 const net      = require('net')
     , sniParse = require('./lib/sni-parse')
+    , floor    = Math.floor
+    , isFunction = require('util').isFunction
+    , isObject   = require('util').isObject
+
+
 
 function parseAddressPort(str) {
     let pos = str.search(/:(\d+)$/)
@@ -19,59 +24,61 @@ function parseAddressPort(str) {
     }
 }
 
+
+
+function createLookupFunction(opts) {
+    if ( isFunction(opts.sni) )
+        return opts.sni
+
+    if ( isObject(opts.sni) ) {
+        let lookup = {}
+
+        for (let hostname in opts.sni)
+            lookup[hostname] = parseAddressPort(opts.sni[hostname])
+
+        return (hostname) => lookup[hostname || '*']
+    }
+
+    throw new Error('opts.sni must be object or function')
+}
+
+
+
 /* createServer({
  *     sni: {
  *         'hostname': 'hostname:port',
  *         ...
+ *         '*':        'fallback-hostname:port'
  *     },
- *     fallback: 'hostname:port',
  *     // options accepted by net.createServer
  * }) => net.Server
- *
- * wildcard / RegExp not supported
  */
 function createServer(opts) {
-    let sni      = opts.sni
-    let fallback = opts.fallback
-    let lookup   = {}
+    const lookupFunc = createLookupFunction(opts)
 
-    if (fallback!==undefined)
-        fallback = parseAddressPort(fallback)
-
-    for (let hostname in sni)
-        lookup[hostname] = parseAddressPort(sni[hostname])
-
-    let server = net.createServer((conn)=>{
-        conn.once('data', (buf)=>{
-
-            let dest
+    let server = net.createServer( (conn)=>{
+        conn
+        .on('error', (err)=>undefined )
+        .once('data', (buf)=>{
             let hostname = sniParse(buf)
-
-            if (hostname === null)
-                dest = fallback
-            else
-                dest = lookup[hostname]
-
-            server.emit('forward', hostname, dest ? dest : {host:null, port:0})
-
-            if (dest===undefined) {
+            let dest = lookupFunc(hostname)
+            if (dest === undefined) {
                 conn.destroy()
             }else{
+                server.emit('forward', hostname, dest)
                 let socket = net.connect(
                     dest.port,
                     dest.host,
                     ()=>{
+                        socket.on('error', (err)=>conn.destroy() )
                         socket.write(buf)
                         socket.pipe(conn)
                         conn.pipe(socket)
-                        socket.on('error', (err)=>conn.destroy() )
                     }
                 )
             }
-
-        })
-        .on('error', (err)=>{} )
-    })
+        } )
+    } )
 
     return server
 }
